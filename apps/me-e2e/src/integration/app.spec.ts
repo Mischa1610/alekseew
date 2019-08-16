3ec67a1215355fe830dbca599e8309c1b6ed4c0d
Michael Alekseew <michael.alekseew@sovanta.com>
2019-08-17 01:11:27 +0200

    feat(app): remove me app and me-e2e, will generate it again new

--- apps/me-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('me', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to me!');
-  });
-});
