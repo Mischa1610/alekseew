8061f4cdc1ccccba9bb461fbe889f26c3b41963e
Michael Alekseew <michael.alekseew@sovanta.com>
2019-08-17 01:42:09 +0200

    refactor(me): move app component files in own app folder in src/app

--- apps/me/src/app/app.module.ts
+++ apps/me/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { AppComponent } from './app/app.component';
+
 @NgModule({
-  declarations: [AppComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
