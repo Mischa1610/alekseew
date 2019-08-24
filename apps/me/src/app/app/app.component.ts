8f2665add1b8bc9b61f693650d7d339e019bc49c
Michael Alekseew <alekseew.m@googlemail.com>
2019-08-25 00:29:49 +0200

    feat(me-app): add general app layout with css grid

--- apps/me/src/app/app/app.component.ts
+++ apps/me/src/app/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'me-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'me';
+  @ViewChild('appShell', { static: true })
+  private appShell!: ElementRef<HTMLDivElement>;
+
+  constructor() {
+    console.log(
+      `%cAPP: ${environment.appTitle}; VERSION: ${
+        environment.appVersion
+      }; ENV-NAME: ${environment.envName}`,
+      'color: blue; font-weight: bold;',
+    );
+  }
 }
